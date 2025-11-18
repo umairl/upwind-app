4878dda9987d09d39a64e1b0ccfad640019d05d5
Umair Latif Ch <163450390+umairl@users.noreply.github.com>
2025-11-18 15:38:46 +0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
+set -e
+
 case ${SERVICE:-suggestion} in
   suggestion)
     cd /app/suggestion
-    . venv/bin/activate
     exec uvicorn app:app --host 0.0.0.0 --port 8000
     ;;
   related)
     cd /app/related
-    . venv/bin/activate
     exec uvicorn app:app --host 0.0.0.0 --port 8001
     ;;
   multiagent)
     cd /app/multiagent
-    . venv/bin/activate
     exec uvicorn app:app --host 0.0.0.0 --port 8002
     ;;
   *)
